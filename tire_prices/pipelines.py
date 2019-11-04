6c8179c51ce206b3996beeed25d6195090276ba0
jimmyhsu1010 <Xukaizhi1010Caimingxuan0220>
2019-11-04 18:01:47 +0800

    Only need to solve the issue of unexpected strings.

--- tire_prices/pipelines.py
+++ tire_prices/pipelines.py
@@ -89,18 +89,26 @@ class ZapaskaPricesPipeline(object):
 
 class PrinterPricePipeline(object):
     def open_spider(self, spider):
-        self.file = open('yandex_price.csv', 'a')
+        self.file = open('regard_price.csv', 'a', encoding='utf-8', newline= '')
         self.writer = csv.writer(self.file)
-        # self.writer.writerow(
-        #     ['Date', 'Company','Brand', 'Season', 'Model', 'Size', 'Diameter', 'Index', 'Price'])
+        self.writer.writerow(
+            ['日期', '品牌', '型號', '價格', '類型', '列印類型', '技術', '每月列印量', '最大尺寸', '自動雙面列印', '最大解析度',
+             '列印速度', '暖機速度', '第一張打印時間', '掃描器類型', '最大掃描尺寸', '掃描解析度', '雙面掃描', '掃描進紙量',
+             '彩色掃描速度', '黑白掃描速度', '複印解析度', '複印速度', '首張複印時間', '紙匣容量', '手動進紙匣容量', '碳粉壽命',
+             '介面', 'Wifi 列印', '直接列印', 'Web介面', '支援作業系統', '顯示類型', '顯示器尺寸', '產品尺寸', '產品重量'])
 
 
     def process_item(self, item, spider):
 
-        self.writer.writerow((item['date'], item['company'], item['brand'],
-                             item['season'], item['model'],
-                             item['size'], item['diameter'], item['index'],
-                             item['price']))
+        self.writer.writerow((item['date'], item['brand'], item['model'], item['price'], item['type'],
+                              item['print_type'], item['print_tech'], item['qt_print_month'], item['max_size'],
+                              item['auto_duplex'], item['max_resolution'], item['print_speed'], item['warm_up'],
+                              item['first_print'], item['scanner_type'], item['scanner_size'], item['scanner_resolution'],
+                              item['scanner_duplex'], item['scanner_capacity'], item['scanner_speed_color'],
+                              item['scanner_speed_b_w'], item['copier_resolution'], item['copier_speed'],
+                              item['first_copier_time'], item['tray_capacity'], item['manual_tray_capacity'], item['toner_life'],
+                              item['interface'], item['wifi_print'], item['direct_print'], item['web_interface'], item['support_os'],
+                              item['display_type'], item['display_size'], item['dimension'], item['weight']))
         return item
 
     def close_spider(self, spider):
--- tire_prices/spiders/printer.py
+++ tire_prices/spiders/printer.py
@@ -8,22 +8,164 @@ import datetime
 
 class PrinterSpider(CrawlSpider):
     name = 'printer'
-    allowed_domains = ['market.yandex.ru']
-    start_urls = ['https://market.yandex.ru/catalog--printery-i-mfu/54546/list?text=многофункциональный%20принтер&hid=138608&srnum=1842&rs=eJwzMjUy5NLn4uVoPM4qwCTBoFpYb23HxcdxYV47qwAjkK_x5yKI_2EXhwCDxFpu1cYGRocARgBV5g0t&glfilter=4914081%3A12110472&glfilter=16826241%3A16826252%2C16826254%2C16826253%2C16826251&onstock=1&local-offers-first=0&page=1']
+    allowed_domains = ['regard.ru']
+    start_urls = ['https://www.regard.ru/catalog/filter/?id=MTgwMDA7MTkyNCw1Mjc1LDUzMTE7MTkyNSw1MjcyOzE5MjYsNTI3MA==&page=1']
 
     custom_settings = {
         'ITEM_PIPELINES': {'tire_prices.pipelines.PrinterPricePipeline': 100}}
 
     rules = (
-        Rule(LinkExtractor(allow=r'https://market.yandex.ru/catalog--printery-i-mfu.*(page=)\d{1,2}'), follow=True),
-        Rule(LinkExtractor(allow=r'https://market.yandex.ru/product--(mfu|printer)-.*'), follow=True),
-        Rule(LinkExtractor(allow=r'https://market.yandex.ru/product--(mfu|printer)-.*(/spec?).*'), callback='parse_item')
+        Rule(LinkExtractor(allow=r'https://www.regard.ru/catalog/filter.*page=\d{1,2}'), follow='True'),
+        Rule(LinkExtractor(allow=r'https://www.regard.ru/catalog/tovar\d*.htm'), callback='parse_item'),
     )
 
     def parse_item(self, response):
         item = PrinterItem()
-        keys = response.xpath('//div[1]/div/dl/dt[1]/span[1]/text()').extract() # Key的位置
-        values = response.xpath('//div[1]/div/dl/dd[1]/span[1]/text()').extract() # Value的位置
-        data = dict(zip(keys, values))
-        print(data)
-        # yield item
+        keys = response.xpath('//div[3]/div[1]/table[1]//tr/td[not(@colspan) and position()=1]/text()').extract() # Key的位置
+        values = response.xpath('//div[3]/div[1]/table[1]//tr/td[not(@colspan) and position()=2]/text()').extract() # Value的位置
+        if 'Внимание ' in keys and len(values) - len(keys) == 3:
+            keys.remove('Внимание ')
+            del values[2:4]
+            data = dict(zip(keys, values))
+        else:
+            data = dict(zip(keys, values))
+        item["date"] = str(datetime.date.today())
+        if 'Производитель' in data.keys():
+            item['brand'] = data['Производитель']
+        else:
+            item['brand'] = 'N/A'
+        if 'Код производителя' in data.keys():
+            item['model'] = data['Код производителя']
+        else:
+            item['model'] = 'N/A'
+        item['price'] = response.xpath('//div[3]/span[2]/span[1]/text()').get().replace(' ', '') + ' руб.'
+        if 'Устройство ' in data.keys():
+            item['type'] = data['Устройство ']
+        else:
+            item['type'] = "N/A"
+        if 'Тип печати ' in data.keys():
+            item['print_type'] = data['Тип печати ']
+        else:
+            item['print_type'] = 'N/A'
+        if 'Технология печати ' in data.keys():
+            item['print_tech'] = data['Технология печати ']
+        else:
+            item['print_tech'] = 'N/A'
+        if 'Количество страниц в месяц ' in data.keys():
+            item['qt_print_month'] = data['Количество страниц в месяц ']
+        else:
+            item['qt_print_month'] = 'N/A'
+        if 'Максимальный формат ' in data.keys():
+            item['max_size'] = data['Максимальный формат ']
+        else:
+            item['max_size'] = 'N/A'
+        if 'Автоматическая двусторонняя печать ' in data.keys():
+            item['auto_duplex'] = data['Автоматическая двусторонняя печать ']
+        else:
+            item['auto_duplex'] = 'N/A'
+        if 'Максимальное разрешение для ч/б печати ' in data.keys():
+            item['max_resolution'] = data['Максимальное разрешение для ч/б печати ']
+        else:
+            item['max_resolution'] = 'N/A'
+        if 'Скорость печати ' in data.keys():
+            item['print_speed'] = data['Скорость печати ']
+        else:
+            item['print_speed'] = 'N/A'
+        if 'Время разогрева ' in data.keys():
+            item['warm_up'] = data['Время разогрева ']
+        else:
+            item['warm_up'] = 'N/A'
+        if 'Время выхода первого отпечатка ' in data.keys():
+            item['first_print'] = data['Время выхода первого отпечатка ']
+        else:
+            item['first_print'] = 'N/A'
+        if 'Тип сканера ' in data.keys():
+            item['scanner_type'] = data['Тип сканера ']
+        else:
+            item['scanner_type'] = 'N/A'
+        if 'Максимальный формат оригинала ' in data.keys():
+            item['scanner_size'] = data['Максимальный формат оригинала ']
+        else:
+            item['scanner_size'] = 'N/A'
+        if 'Разрешение сканера ' in data.keys():
+            item['scanner_resolution'] = data['Разрешение сканера ']
+        else:
+            item['scanner_resolution'] = 'N/A'
+        if 'Устройство автоподачи оригиналов ' in data.keys():
+            item['scanner_duplex'] = data['Устройство автоподачи оригиналов ']
+        else:
+            item['scanner_duplex'] = 'N/A'
+        if 'Емкость устройства автоподачи оригиналов ' in data.keys():
+            item['scanner_capacity'] = data['Емкость устройства автоподачи оригиналов ']
+        else:
+            item['scanner_capacity'] = 'N/A'
+        if 'Скорость сканирования (цветн.) ' in data.keys():
+            item['scanner_speed_color'] = data['Скорость сканирования (цветн.) ']
+        else:
+            item['scanner_speed_color'] = 'N/A'
+        if 'Скорость сканирования (ч/б) ' in data.keys():
+            item['scanner_speed_b_w'] = data['Скорость сканирования (ч/б) ']
+        else:
+            item['scanner_speed_b_w'] = 'N/A'
+        if 'Максимальное разрешение копира (ч/б) ' in data.keys():
+            item['copier_resolution'] = data['Максимальное разрешение копира (ч/б) ']
+        else:
+            item['copier_resolution'] = 'N/A'
+        if 'Скорость копирования ' in data.keys():
+            item['copier_speed'] = data['Скорость копирования ']
+        else:
+            item['copier_speed'] = 'N/A'
+        if 'Время выхода первой копии ' in data.keys():
+            item['first_copier_time'] = data['Время выхода первой копии ']
+        else:
+            item['first_copier_time'] = 'N/A'
+        if 'Подача бумаги ' in data.keys():
+            item['tray_capacity'] = data['Подача бумаги ']
+        else:
+            item['tray_capacity'] = 'N/A'
+        if 'Емкость лотка ручной подачи ' in data.keys():
+            item['manual_tray_capacity'] = data['Емкость лотка ручной подачи ']
+        else:
+            item['manual_tray_capacity'] = 'N/A'
+        if 'Ресурс ч/б картриджа/тонера ' in data.keys():
+            item['toner_life'] = data['Ресурс ч/б картриджа/тонера ']
+        else:
+            item['toner_life'] = 'N/A'
+        if 'Интерфейсы ' in data.keys():
+            item['interface'] = data['Интерфейсы ']
+        else:
+            item['interface'] = 'N/A'
+        if 'Поддержка AirPrint ' in data.keys():
+            item['wifi_print'] = data['Поддержка AirPrint ']
+        else:
+            item['wifi_print'] = 'N/A'
+        if 'Прямая печать ' in data.keys():
+            item['direct_print'] = data['Прямая печать ']
+        else:
+            item['direct_print'] = 'N/A'
+        if 'Веб-интерфейс ' in data.keys():
+            item['web_interface'] = data['Веб-интерфейс ']
+        else:
+            item['web_interface'] = 'N/A'
+        if 'Поддержка ОС ' in data.keys():
+            item['support_os'] = data['Поддержка ОС ']
+        else:
+            item['support_os'] = 'N/A'
+        if 'ЖК панель (дисплей) ' in data.keys():
+            item['display_type'] = data['ЖК панель (дисплей) ']
+        else:
+            item['display_type'] = 'N/A'
+        if 'Диагональ дисплея ' in data.keys():
+            item['display_size'] = data['Диагональ дисплея ']
+        else:
+            item['display_size'] = 'N/A'
+        if 'Размеры ' in data.keys():
+            item['dimension'] = data['Размеры ']
+        else:
+            item['dimension'] = 'N/A'
+        if 'Вес' in data.keys():
+            item['weight'] = data['Вес']
+        else:
+            item['weight'] = 'N/A'
+        # print(item)
+        yield item
